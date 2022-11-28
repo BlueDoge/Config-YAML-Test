d4a21ff0475ff2102ae15c53837d116a116eb052
Elizabeth Clements <BlueDoge@users.noreply.github.com>
2022-11-28 14:10:04 -0800

    first code

---
+++ Configurator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigYamlTest
+{
+	public class Configurator
+	{
+		private IConfigurationRoot Data;
+
+		public Configurator()
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(AppContext.BaseDirectory)
+				.AddYamlFile("Config.yaml");
+			Data = builder.Build();
+		}
+
+		public IConfigurationRoot GetData() => Data;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ConfigYamlTest;
+using Microsoft.Extensions.Configuration;
+
+namespace BlueDoge.Bot
+{
+	[Serializable]
+	public class CommandLineArgumentsException : Exception
+	{
+	}
+
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var config = new Configurator();
+			var data = config.GetData();
+			Console.WriteLine(data.GetDebugView());
+		}
+	}
+}
